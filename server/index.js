2af4e3bed992a448bd3605120e475c5edcf3d8c4
CHAZ peters <stancp327@gmail.comgit config --global user.name>
2025-03-24 04:15:40 -0700

    Setup MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 10000;
+const MONGO_URI = process.env.MONGO_URI;
 
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
+// Simple route
 app.get('/', (req, res) => {
-  res.send('FetchWork backend running');
+  res.send('FetchWork backend running with MongoDB');
 });
 
 app.listen(PORT, () => {
