7e21e4912363be96ace5a6bce3ddddcbcb41a497
stancp327 <stancp327@gmail.com>
2026-03-12 23:21:13 -0700

    perf(lcp): add react-snap for homepage pre-rendering and inline critical CSS

---
+++ client/react-snap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  include: ['/'],
+  inlineCss: true,
+  removeBlobs: true,
+  removeStyleTags: false,
+  removeScriptTags: false,
+  minifyHtml: true
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { hydrateRoot, createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes() && rootElement.innerHTML.trim().length > 0) {
+  hydrateRoot(rootElement, <React.StrictMode><App /></React.StrictMode>);
+} else {
+  const root = createRoot(rootElement);
+  root.render(<React.StrictMode><App /></React.StrictMode>);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
