e900a821c6bba81f2f9f45524110d33f60531ef8
CHAZ peters <stancp327@gmail.comgit config --global user.name>
2026-03-18 17:03:16 -0700

    fix(webhooks): remove _id:false from ProcessedWebhookEvent to fix insert error

--- server/models/ProcessedWebhookEvent.js
+++ server/models/ProcessedWebhookEvent.js
@@ -9,7 +9,7 @@ const schema = new mongoose.Schema({
   stripeEventId: { type: String, required: true, unique: true, index: true },
   type:          { type: String, required: true },
   processedAt:   { type: Date,   default: Date.now },
-}, { _id: false });
+});
 
 schema.index({ processedAt: 1 }, { expireAfterSeconds: 30 * 24 * 60 * 60 }); // 30-day TTL
 
